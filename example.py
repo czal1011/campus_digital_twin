0efa72a88c666d191be700130ec57fd70a59e9f7
None <None>
2024-06-20 13:03:34 +0200

    Added Peter

--- example.py
+++ example.py
@@ -8,6 +8,8 @@ from mininet.util import dumpNodeConnections
 from mininet.log import lg
 from mininet.cli import CLI
 
+import sys
+
 """
 1 Controller
 2-3 Switches
@@ -41,18 +43,15 @@ def createTopo(sdn_controller, **kwargs):
 	return Mininet(topo=topo, link=TCLink, controller=sdn_controller, **kwargs)
 
 def modifyTopo(net):
-	topo = net.topo
-	peter = topo.addHost('peter', ip='10.0.0.10')
-	topo.addLink(peter, 's2')
-
-if __name__ == '__main__':
-	c = OVSController("controller")
-	lg.setLogLevel('info')
-	net = createTopo(c, listenPort=6654)
-	net.start()
-	# print("Testing bandwidth between client and server (iperf)")
-	# net.iperf(net.get("client", "server"))
-	# print("iperf test done, starting CLI")
+	s2 = net.get('s2')
+	# adding Peter
+	peter = net.addHost('peter', ip='10.0.0.10/26')
+	net.addLink(peter, 's2')
+	s2.attach('s2-eth3')
+	peter.cmdPrint('ifconfig peter-eth0 10.0.0.10')
+	
+def dumpNodeIPandMAC(net):
+	c = net['controller'] # controller
 	print(f"Controller IP/Port: {c.ip}:{c.port}")
 	print(f"Hosts ({len(net.hosts)}):")
 	for host in net.hosts:
@@ -60,14 +59,44 @@ if __name__ == '__main__':
 		for port in host.ports:
 			print(f"{port}", end='')
 		print() # new line
-	#CLI(net)
-	print("CLI closed, dumping flows...")
-	c.cmdPrint(f"dpctl dump-flows tcp:localhost:6654")
 
+def dumpFlows(controller, cliClosed):
+	if cliClosed is False:
+		print("Dumping flows before opening CLI...")
+	else:
+		print("CLI closed, dumping flows...")
+	c.cmdPrint(f"dpctl dump-flows tcp:localhost:6654")
+	print("-----------------------------------------------------")
+	c.cmdPrint(f"ovs-ofctl dump-flows s2")
+	print("Dumping complete, starting CLI...\n",
+		  "=====================================================")
+
+if __name__ == '__main__':
+	argv = sys.argv # get extra parameters
+	
+	c = OVSController("controller")
+	lg.setLogLevel('info')
+	net = createTopo(c, listenPort=6654)
+	net.start()
+	# print("Testing bandwidth between client and server (iperf)")
+	# net.iperf(net.get("client", "server"))
+	# print("iperf test done, starting CLI")
+	dumpNodeIPandMAC(net)
+	
 	print("Adding another node...")
-	peter = net.topo.addHost('peter', ip='10.0.0.10')
-	net.topo.addLink(peter, 's2')
+	modifyTopo(net)
+	
+	print(f"s2 Interfaces:")
+	net.get('s2').cmdPrint('ifconfig')
+	
+	dumpFlows(c, cliClosed = False)
+	
+	if(len(argv) > 1 and argv[1] == '--blockPeter'):
+		net.get('s2').cmdPrint('ovs-ofctl add-flow s2 nw_dst=10.0.0.10,actions=drop') # block Peter
+	
 	CLI(net)
+	
+	dumpFlows(c, cliClosed = True)
 	
 	print("\n### Stopping Mininet...\n")
 	net.stop()
